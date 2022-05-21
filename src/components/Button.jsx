777cb1d8535522351ecd8757cf48ff68268dd028
Gaston Danielsen <gastondanielsen85@gmail.com>
2022-05-21 00:34:01 -0300

    Setup Proyect Sass

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './button.module.scss';
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>Button</div>
+  )
+}
+
+export default Button
